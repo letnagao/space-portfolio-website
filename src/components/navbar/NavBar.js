b60c8c14f1dedab40155bd690b453cde55f5a6cf
letnagao <leticia-nagao@hotmail.com>
2022-07-12 22:44:07 -0300

    first commit

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,27 @@
+import { Navbar, Container } from 'react-bootstrap';
+
+export const NavBar = () => {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img src={''} alt="Logo" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#skills">Skills</Nav.Link>
+                    <Nav.Link href="#projects">Projects</Nav.Link>
+                </Nav>
+                <span className="navbar-text">
+                    <div className="social-icon">
+
+                    </div>
+                </span>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
